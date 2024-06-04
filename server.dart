814a3bb93711bf55666e7a603909df7f576f15b5
Saurav Gautam <55937631+sauravgautam320@users.noreply.github.com>
2024-06-04 10:07:25 +0545

    Update server.dart

--- server.dart
+++ server.dart
@@ -3,8 +3,23 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 import 'routes/index.dart' as api;
 
+Response _handleCors(RequestContext context, Response response) {
+  return response.copyWith(
+    headers: {
+      ...response.headers,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  );
+}
+
+Handler middleware(Handler handler) {
+  return handler.use(requestLogger()).use(cors(_handleCors));
+}
+
 Future<HttpServer> runServer() {
-  final handler = api.onRequest;
+  final handler = middleware(onRequest);
   return serve(
     handler,
     InternetAddress.anyIPv4,
